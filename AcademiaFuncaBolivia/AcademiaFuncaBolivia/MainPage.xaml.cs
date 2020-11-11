ea134d886f7769f0a4c489d7a61fef780ebef46d
Samleinav <samleighton02@gmail.com>
2020-11-10 22:33:41 -0600

    Initial commit

---
+++ AcademiaFuncaBolivia/AcademiaFuncaBolivia/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Android.Webkit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace AcademiaFuncaBolivia
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.academiaView.Source = new UrlWebViewSource() { Url = "https://academia.funcabolivia.com/" };
+            var cookieHeader = CookieManager.Instance.GetCookie("https://academia.funcabolivia.com/");
+        }
+    }
+}
